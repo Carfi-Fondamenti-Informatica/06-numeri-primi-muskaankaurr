a2550440b42b05368bd080372dd7c3d4f9a5909e
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-02 19:15:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+# include "lib.h"
+using namespace std;
 int main(){
+  int a=0;
+  cin >> a;
+  if (numeroprimo(a)){
+    cout << "numero primo" << endl,
+  }else {
+    cout << "numero non primo" << emdl;
+ }  
   return 0;
 }
