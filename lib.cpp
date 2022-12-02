fa80cf2448791c59627fcbed7faad5523a911e95
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-02 19:18:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
-bool numeroprimo(int a);
+#include "libreria.h"
+using namespace std;
+
+bool numeroprimo ( int a ){
+    bool segnalibro = false;
+    int i=2;
+    if ( a != 2 ){
+        for (;i<a ; i++){
+            if (a%i==0){
+                break;
+            }
+        }
+    }else{
+        segnalibro=true;
+    }
+   if (i==a){
+       segnalibro=true;
+   }
+return segnalibro;
+}
+
+
